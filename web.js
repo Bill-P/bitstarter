a8d658df3e7c5f134b443fdbef903eefeb383179
Bill Pecor <williampecor@hotmail.com>
2013-07-10 04:25:19 +0000

    Remove unnessary console.log

--- web.js
+++ web.js
@@ -1,4 +1,4 @@
-var express = require('express');
+Var! express = require('express');
 
 var app = express.createServer(express.logger());
 
@@ -10,6 +10,5 @@ app.get('/', function(request, response) {
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log(fs.readFileSync("index.html").toString());
   console.log("Listening on " + port);
 });
